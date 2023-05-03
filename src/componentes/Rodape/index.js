ecb7824b0028639ee2b9cb63843c018f29727aea
Mauro Rocha <maurort@apoioprodesp.sp.gov.br>
2023-05-03 20:39:30 -0300

    Primeiro commit fork do organograma v1

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,35 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer className='footer'>
+            <section className='social-media'>
+                <ul>
+                    <li>
+                        <a href="" target='_blank'>
+                            <img src='/imagens/fb.png' alt='facebook' />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" target='_blank'>
+                            <img src='/imagens/tw.png' alt='Twitter' />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" target='_blank'>
+                            <img src='/imagens/ig.png' alt='Instagram' />
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <img src='/imagens/logo.png' alt='logotipo' />
+            </section>
+            <section className='desenvolvido'>
+                <p>Desenvolvido por Alura</p>
+            </section>
+        </footer>
+    )
+}
+
+export default Rodape
