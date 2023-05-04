de7741924cf23a8fb6a611e4937f77ba44168b1b
Mauro Rocha <mauro.rocha.t@gmail.com>
2023-05-03 22:10:31 -0300

    alt - Novos times, Cores primaria e secundárias

--- src/App.js
+++ src/App.js
@@ -8,39 +8,34 @@ function App() {
 
   const times = [
     {
-      nome: 'Programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9',
+      nome: 'Azul',
+      corPrimaria: '#1c92c9',
+      corSecundaria: '#c8ebff',
     },
     {
-      nome: 'Front-End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF',
+      nome: 'Branco',
+      corPrimaria: '#ffcb65',
+      corSecundaria: '#fff4e6',
     },
     {
-      nome: 'Data Sciense',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2',
+      nome: 'Incolor',
+      corPrimaria: '#cccccc',
+      corSecundaria: '#eeeeee',
     },
     {
-      nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      nome: 'Verde',
+      corPrimaria: '#1cc940',
+      corSecundaria: '#a6ffd1',
     },
     {
-      nome: 'UX e Design',
-      corPrimaria: '#D86EBF',
-      corSecundaria: '#FAE5F5',
+      nome: 'Vermelho',
+      corPrimaria: '#ff6600',
+      corSecundaria: '#ffbcb2'
     },
     {
-      nome: 'Mobile',
-      corPrimaria: '#FEBA05',
-      corSecundaria: '#FFF5D9',
-    },
-    {
-      nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF',
+      nome: 'Preto',
+      corPrimaria: '#1d1d1d',
+      corSecundaria: '#515b59',
     }
   ]
 
