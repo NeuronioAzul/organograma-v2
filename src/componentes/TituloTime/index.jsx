648d4c40f02848b9a3d27b637f23e19af600e1fd
Mauro Rocha <maurort@apoioprodesp.sp.gov.br>
2023-05-07 19:08:27 -0300

    add - função click ao bt adicionar

--- src/componentes/TituloTime/index.jsx
+++ src/componentes/TituloTime/index.jsx
@@ -1,10 +1,14 @@
 import './TituloTime.css'
 import { BiAddToQueue } from 'react-icons/bi'
 
-const TituloTime = () => {
+const TituloTime = ({ tratarExibir }) => {
     return (
         <div className='titulo-time'>
-            <BiAddToQueue size={40} className='adicionar' />
+            <BiAddToQueue
+                onClick={tratarExibir}
+                size={40}
+                className='adicionar'
+            />
             <h2>Times por Cor de Mana:</h2>
             <hr />
         </div>
